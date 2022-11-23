7c4710339804a5841580a27dc2156e6d0fac7738
Alexandr Smirnov <smirnov.togar@ya.ru>
2022-11-23 23:22:37 +0400

    fix

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { Telegraf } from 'telegraf';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.on('text', async (ctx) => {
-  // Using context shortcut
-  await ctx.reply(`Hello`);
-});
+// bot.on('text', async (ctx) => {
+//   // Using context shortcut
+//   await ctx.reply(`Hello`);
+// });
 
 bot.command('new', async (ctx) => {
   await ctx.reply('you sent:' + ctx.message.text);
